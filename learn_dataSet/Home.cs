08c8991b7514c9e391d69c37e2bee10ac3ee5b4d
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-12-01 20:32:38 +0100

    start doing stuf

---
+++ learn_dataSet/Home.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learn_dataSet
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        void Changer_Form(Form NewForm)
+        {
+            if (this.ActiveMdiChild != null) this.ActiveMdiChild.Close();
+            NewForm.MdiParent = this;
+            NewForm.Dock = DockStyle.Fill;
+            NewForm.Show();
+        }
+
+        private void navigationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Changer_Form(new Navigation_Chauffeur());
+        }
+    }
+}
---
+++ learn_dataSet/Navigation_Véhicule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learn_dataSet
+{
+    public partial class Navigation_Véhicule : Form
+    {
+        public Navigation_Véhicule()
+        {
+            InitializeComponent();
+        }
+    }
+}
